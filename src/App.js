f39f1931dce2ec2a89eafd9d86cb462456bd0a43
Muhammad Arhum <arhumsharif06@gmail.com>
2022-12-10 19:48:39 +0500

    Project Updated

--- src/App.js
+++ src/App.js
@@ -11,70 +11,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // Components
 import Login from './components/Credentials/Login';
-import DaySheets from './components/DaySheets/DaySheets';
-import Reports from './components/Reports/Reports';
-import Invoicing from './components/Invoicing/Invoicing';
-import PendingPayment from './components/PendingPayment/PendingPayment'
-import FindJobs from './components/Jobs/FindJobs';
-import AddJobs from './components/Jobs/AddJobs';
-import FindCustomers from './components/Customer/FindCustomers';
-import AddCustomers from './components/Customer/AddCustomers';
-import Profile from './components/Profile/Profile';
-import Settings from './components/Settings/Settings';
-import AddEmployee from './components/Employee/AddEmployee';
-import FindEmployee from './components/Employee/FindEmployee';
-// Layout
-import Layout from './components/Layout/Layout';
 import Signup from './components/Credentials/Signup';
-// SuperAdmin
-import Dashboard from './components/SuperAdmin/Dashboard/Dashboard';
-import Coupons from './components/SuperAdmin/Coupons/Coupons'
-import Payment from './components/SuperAdmin/Payment/Payment'
-import Plans from './components/SuperAdmin/Plans/Plans'
-import PaymentDetails from './components/Credentials/PaymentDetails';
-import PurchaseDetails from './components/Credentials/PurchaseDetails';
 
-// Higher Ordered Components Used
-const DaySheetsComponent = Layout(DaySheets)
-const ReportsComponent = Layout(Reports)
-const InvoicingComponent = Layout(Invoicing)
-const JobsComponent = Layout(AddJobs)
-const FindJobsComponent = Layout(FindJobs)
-const FindCustomersComponent = Layout(FindCustomers)
-const AddCustomersComponent = Layout(AddCustomers)
-const ProfileComponent = Layout(Profile)
-const SettingsComponent = Layout(Settings)
-const AddEmployeeComponent = Layout(AddEmployee)
-const FindEmployeeComponent = Layout(FindEmployee)
-const PendingPaymentComponent = Layout(PendingPayment)
-const SuperAdminDashboard = Layout(Dashboard)
-const CouponsAdminComponent = Layout(Coupons)
-const PaymentAdminComponent = Layout(Payment)
-const PlansAdminComponent = Layout(Plans)
+// Higher order component
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/day-sheets" element={<DaySheetsComponent />} />
-        <Route path="/reports" element={<ReportsComponent />} />
-        <Route path="/invoicing" element={<InvoicingComponent />} />
-        <Route path="/pending" element={<PendingPaymentComponent />} />
-        <Route path="/add-jobs" element={<JobsComponent />} />
-        <Route path="/find-jobs" element={<FindJobsComponent />} />
-        <Route path="/find-customers" element={<FindCustomersComponent />} />
-        <Route path="/add-customers" element={<AddCustomersComponent />} />
-        <Route path="/profile" element={<ProfileComponent />} />
-        <Route path="/find-employee" element={<FindEmployeeComponent />} />
-        <Route path="/add-employee" element={<AddEmployeeComponent />} />
-        <Route path="/settings" element={<SettingsComponent />} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/payment-details" element={<PaymentDetails/>} />
-        <Route path="/purchase-details" element={<PurchaseDetails/>} />
-        <Route path="/admin-dashboard" element={<SuperAdminDashboard />} />
-        <Route path="/admin-coupons" element={<CouponsAdminComponent />} />
-        <Route path="/admin-payment" element={<PaymentAdminComponent />} />
-        <Route path="/admin-plans" element={<PlansAdminComponent />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
--- src/components/Credentials/Signup.js
+++ src/components/Credentials/Signup.js
@@ -1,88 +1,9 @@
-// <!--
-// ============================================================================
-// * Iron Motorsports V1.0
-// ============================================================================
-// * Coded by WeTeck - Talha Noman
-// ============================================================================
-// -->
-
-import React, { useState, useRef } from "react";
-import { addUser } from "../../api";
-import { Link, useNavigate } from "react-router-dom";
-import Logo from "../../assets/Logo.png";
+import React from "react";
 export default function Signup() {
 
-    let navigate = useNavigate()
-
-    const username = useRef();
-    const password = useRef();
-    const [login, setlogin] = useState("hidden");
-    const [message, setMessage] = useState("hidden");
-
-
-    // Button will be displayed iff fields are not left empty.
-    const handleLogin = () => {
-        if (username.current.value && password.current.value) setlogin("block");
-        else setlogin("hidden");
-    };
-
-    const register = async () => {
-        let response = await addUser(username.current.value,password.current.value);
-        if (response == 404)
-        {
-            setMessage('block')
-        }
-        else
-        {
-            console.log(response.user_guid)
-            navigate("/payment-details", {state: {user_guid: response.user_guid}})
-        }
-      };
-
     return (
-        <div className="flex min-h-screen justify-center items-center">
-            <div className="w-full shadow-lg max-w-lg">
-                <div className="rounded-2xl p-10 bg-white text-center font-semibold text-xs">
-                    {/* Logo */}
-                    <img src={Logo} alt="" className="mx-auto mb-10" />
-                    {/* Message */}
-                    {/* Message */}
-                    <div className={`mb-4 ${message}`}>
-                        <label className="border border-solid border-[#FF0000] px-6 py-2 text-center font-semibold text-lg rounded-xl">
-                        Admin Exists Already!
-                        </label>
-                    </div>
-                    <input
-                        type="text"
-                        placeholder="username"
-                        autoComplete="given-name"
-                        className="my-3 focus:ring-slate-500 focus:border-slate-500 block shadow-sm sm:text-sm border-gray-300 rounded-xl w-64 h-12 mx-auto placeholder:font-semibold"
-                        ref={username}
-                        onChange={handleLogin}
-                    />
-                    <input
-                        type="password"
-                        placeholder="password"
-                        autoComplete="given-name"
-                        className="mt-3 focus:ring-slate-500 focus:border-slate-500 block shadow-sm sm:text-sm border-gray-300 rounded-xl w-64 h-12 mx-auto placeholder:font-semibold"
-                        ref={password}
-                        onChange={handleLogin}
-                    />
-                    <div className="flex space-x-2 justify-center">
-                            <button
-                                onClick={register}
-                                type="button"
-                                className={`${login} inline-block px-6 py-2.5 mt-5 bg-slate-900 text-white font-medium text-xs leading-tight uppercase rounded-xl shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-slate-800 active:shadow-lg transition duration-150 ease-in-out`}
-                            >
-                                Signup
-                            </button>
-                    </div>
-                    <h6 className="mt-7">Forgot Password?</h6>
-                    <h6 className="mb-4">Contact support@ironindustries.com.au</h6>
-                    <h6>Already have an account</h6>
-                    <Link to='/'>Log in <span className="underline decoration-solid">here</span></Link>
-                </div>
-            </div>
+        <div>
+            Sign Up page
         </div>
     );
 }
