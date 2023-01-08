343a1624e220b1c6b571e3880b0b5ff0325367da
Abdullah Tariq <mat57557@gmail.com>
2023-01-08 17:38:52 +0500

    Navbar Updated

--- src/components/Navbar/TourGuideNavbar.js
+++ src/components/Navbar/TourGuideNavbar.js
@@ -6,77 +6,96 @@
 // ============================================================================
 // -->
 
-import React from "react";
-import Logo from "../../assets/realLogo.png";
+import React from 'react';
+import Logo from '../../assets/realLogo.png';
 // import EmailIcon from '../../assets/Email.svg'
-import { useLocation, Link, useNavigate } from "react-router-dom";
-import Cookies from "universal-cookie";
+import { useLocation, Link, useNavigate } from 'react-router-dom';
+import Cookies from 'universal-cookie';
 export default function TourGuideNavbar() {
-  let title = "";
+  let title = '';
   const { pathname } = useLocation();
   const cookies = new Cookies();
-  let token = cookies.get("token");
+  let token = cookies.get('token');
 
   let navigate = useNavigate();
 
-
   return (
     <div>
-        {/* Navbar */}
-
-        <nav className="fixed top-0 w-full z-50 bg-gray-800 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800">
-            <div className="container flex flex-wrap items-center justify-between mx-auto">
-                <Link to="" className="flex items-center">
-                    <img src={Logo} className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Travelmania</span>
-                </Link>
-                <div className="flex md:order-2">
-                    <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1" >
-                        <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-                        <span className="sr-only">Search</span>
-                    </button>
-                    <div className="relative hidden md:block">
-                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-                            <span className="sr-only">Search icon</span>
-                        </div>
-                        <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
-                    </div>
-                    <button data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
-                        <span className="sr-only">Open menu</span>
-                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                    </button>
-                    {/* Logout icon */}
-                    <div onClick={() => {
-                        cookies.set('token', '')
-                        navigate('/login')
-                    }} className="mt-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-10 h-7 text-white ">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
-                        </svg>
-
-                    </div>
-                </div>
-                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
-                    <div className="relative mt-3 md:hidden">
-                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-                        </div>
-                        <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
-                    </div>
-                    <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li>
-                            <Link to="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</Link>
-                        </li>
-                        <li>
-                            <Link to="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</Link>
-                        </li>
-                    </ul>
-                </div>
+      <nav className='fixed top-0 w-full z-50 bg-gray-800 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800'>
+        <div className='container flex flex-wrap items-center justify-between mx-auto'>
+          <Link to='' className='flex items-center'>
+            <img src={Logo} className='h-6 mr-3 sm:h-9' alt='Flowbite Logo' />
+            <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
+              Travelmania
+            </span>
+          </Link>
+          <div class='flex md:order-2'>
+            <button
+              onClick={() => {
+                cookies.set('token', '');
+                navigate('/login');
+              }}
+              type='button'
+              class='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-800 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+            >
+              Log Out
+            </button>
+          </div>
+          <div
+            className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
+            id='navbar-search'
+          >
+            <button
+              type='button'
+              data-collapse-toggle='navbar-search'
+              aria-controls='navbar-search'
+              aria-expanded='false'
+              className='md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1'
+            >
+              <svg
+                className='w-5 h-5'
+                aria-hidden='true'
+                fill='currentColor'
+                viewBox='0 0 20 20'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  fill-rule='evenodd'
+                  d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
+                  clip-rule='evenodd'
+                ></path>
+              </svg>
+              <span className='sr-only'>Search</span>
+            </button>
+            <div className='relative hidden md:block'>
+              <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
+                <svg
+                  className='w-5 h-5 text-gray-500'
+                  aria-hidden='true'
+                  fill='currentColor'
+                  viewBox='0 0 20 20'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    fill-rule='evenodd'
+                    d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
+                    clip-rule='evenodd'
+                  ></path>
+                </svg>
+                <span className='sr-only'>Search icon</span>
+              </div>
+              <input
+                type='text'
+                id='search-navbar'
+                className='block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                placeholder='Search...'
+              />
             </div>
-        </nav>
+          </div>
+        </div>
+      </nav>
 
-        {/* Navbar end */}
+      {/* Navbar end */}
     </div>
   );
 }
