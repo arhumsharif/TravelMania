e146fe622e30d5f7b7dba07a35069b2b77d45d09
Abdullah Tariq <mat57557@gmail.com>
2023-01-21 03:56:16 +0500

    completed maps
    maps is completed till now but the main concern is the billing account for the google maps

--- src/components/LocationBasedSearch/Map/Map.jsx
+++ src/components/LocationBasedSearch/Map/Map.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import { LocationOnOutlined } from '@material-ui/icons';
+import { Rating } from '@material-ui/lab';
+import useStyles from './styles';
+import { geolocated } from 'react-geolocated';
 
 const Map = () => {
-  return <h1>Map</h1>;
+  const [coordinates, setCurrentLocation] = useState({});
+  useEffect(() => {
+    getLocation();
+  }, []);
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      setCurrentLocation({ latitude, longitude });
+    });
+  };
+  // const coordinates = {lat: 0, lng: 0};
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyAS0fabteoQUtd14T1BoeTinzVAU6tndKk' }}
+        defaultCenter={{
+          lat: coordinates.latitude,
+          lng: coordinates.longitude,
+        }}
+        center={{
+          lat: coordinates.latitude,
+          lng: coordinates.longitude,
+        }}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
