862411bd55665f2733a8bf2c0ba89e90ef17ee39
Abdullah Tariq <mat57557@gmail.com>
2023-02-05 01:29:16 +0500

    updated minor changes

--- src/components/Credentials/Login.js
+++ src/components/Credentials/Login.js
@@ -111,7 +111,7 @@ const Login = ({ setAlert }) => {
                           onClick={login}
                           type='button'
                           style={{ transition: 'all .15s ease' }}
-                          className='bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full'
+                          className='bg-gray-900 text-white hover:bg-orange-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full'
                         >
                           Login
                         </button>
