6b58df8940302eb07625f744cb9dd0d900c8e4fa
Abdullah Tariq <mat57557@gmail.com>
2023-04-01 20:59:29 +0500

    solved issues
    -> google maps api call back
    -> user being logged out when the page refreshes

--- src/store.js
+++ src/store.js
@@ -2,15 +2,25 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-const initialState = {};
+const presistConfig = {
+  key: 'root',
+  storage,
+};
 
-const middleware = [thunk];
+const persistedReducer = persistReducer(presistConfig, rootReducer);
+const store = createStore(persistedReducer, applyMiddleware(thunk));
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+// const initialState = {};
+
+// const middleware = [thunk];
+
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// );
 
 export default store;
